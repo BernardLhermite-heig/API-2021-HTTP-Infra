4e1e93c2f184491ac4b0a60bc35a0bb55bfa1ed1
BernardLhermite <stephane.marengo@hotmail.com>
2021-12-19 11:02:13 +0100

    Ajout d'un timeout dans la requête AJAX

--- docker-images/apache-ajax/content/assets/js/prize.js
+++ docker-images/apache-ajax/content/assets/js/prize.js
@@ -4,10 +4,14 @@ $(() => {
     function loadPrize() {
         var text;
 
-        $.getJSON("/api/prize/", prize => {
+        $.ajax({
+            url: "/api/prize/",
+            dataType: 'json',
+            timeout: 3000
+        }).done(prize => {
             console.log(prize);
             text = "Vous avez reçu un " + prize.name
-            + " d'une valeur de " + prize.value + " !";
+                    + " d'une valeur de " + prize.value + " !";
         }).fail((jqxhr, textStatus, error) => {
             console.log("Request Failed: " + textStatus + ", " + error);
             text = "Vous n'avez rien reçu )-8";
